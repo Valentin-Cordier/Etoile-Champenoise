70ce6aff151646ebb7a64823045683600b3888f4
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-05-07 15:37:54 +0200

    back esapce suivi

--- espace.php
+++ espace.php
@@ -13,6 +13,7 @@
 </head>
 <body>
   <?php include('header.php');?>
+  <?php include "inc/co.php"?>
 
   <img src="images/mbsP.png" class="img-fluid" style="margin-top: 5em; height: 50vh; width:100%; object-fit:cover;" alt="Responsive image de livraison">
     <div class="container-fluid col-12 text-center mt-3">
@@ -22,37 +23,20 @@
     </div>
     <div class="container-fluid col-12">
         <select class="custom-select col-6 text-center offset-3 mt-5">
-            <option selected>Sélection de votre partenaire</option>
-            <option value="1">Sellier(02)</option>
-            <option value="2">MBS 08 (08)</option>
-            <option value="3">TCP (10)</option>
-            <option value="4">GTRA 89 (13)</option>
-            <option value="5">GTRA 59 (13)</option>
-            <option value="6">GTRA 01(13)</option>
-            <option value="7">GTRA 95 (13)</option>
-            <option value="8">Cogepress (16)</option>
-            <option value="9">VARILLON (27)</option>
-            <option value="10">MESSAGERIE TOULOUSAINE (31)</option>
-            <option value="11">BENITO (33)</option>
-            <option value="12">OENOTRANS (51)</option>
-            <option value="13">GONDRAND (52)</option>
-            <option value="14">IMANY (52)</option>
-            <option value="15">HEPPNER 57 (57)</option>
-            <option value="16">HEPPNER 59 (59)</option>
-            <option value="17">MERY TRANSPORTS (60)</option>
-            <option value="18">HEPPNER 67 (67)</option>
-            <option value="19">HEPPNER 68 (68)</option>
-            <option value="20">LAMBERT ET VALETTE (69)</option>
-            <option value="21">TRANSSERVICE (76)</option>
-            <option value="22">XP (77)</option>
-            <option value="23">CST PROFRET (80)</option>
-            <option value="24">TMV (88)</option>
-            <option value="25">TRSP DE L'YONNE (89)</option>
-            <option value="26">UPSILON (89)</option>
-            <option value="27">GTLS75 (94)</option>
-            <option value="28">TDL (95)</option>
-            <option value="29">CLL (95)</option>
-            <option value="30">HEPPNER 95 (95)</option>
+           <?php     $req = $dbh->prepare('SELECT * FROM user WHERE IDuser != 40 AND zip != 00000 ORDER BY zip ');
+                        $req->execute();
+                    while ($donnees = $req->fetch()){
+                        $var1 = $donnees['IDuser'];
+                        $var2 = $donnees['nom'];
+                        $var3 = $donnees['zip'];
+                        $dep =  substr ( $var3 , 0 , 2);
+                            if ($var1 == $id){
+                                echo '<option value="'.$var1.'" selected>'.$var2.'('.$dep.')</option>';
+                            } else {
+                                echo '<option value="'.$var1.'">'.$var2.'('.$dep.')</option>';
+                            }
+                        }
+                        ?>
         </select>
     </div>
     <div class="container-fluid text-center mt-5">
@@ -61,10 +45,22 @@
             <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
                 <button type="submit" class="btn btn-primary btn-lg mb-5">Rechercher</button>
             </div>
-            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12"></div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+              <?php $req = $dbh->prepare('SELECT * FROM user WHERE IDuser = "'.$id.'" ');
+                    $req->execute();
+                while ($donnees = $req->fetch()){
+                    $var1 = $donnees['link'];
+                    $var2 = $donnees['nom'];
+                    echo '<br><p class="mid">(Cliquez ci-dessous si la redirection automatique ne fonctionne pas)</p><br>';
+                    echo '<p class="mid"><a class="ralink" href="'.$var1.'" target="_blank">Lien tracking '.$var2.'</a><p><br>';
+                    echo '<script language="javascript">
+                    window.open("'.$var1.'", "_blank");
+             </script>';
+                        }
+                         ?>
+            </div>
         </div>
     </div>
-
   <?php include('footer.php');?>
 </body>
 </html>
