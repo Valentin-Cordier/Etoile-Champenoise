56939a0b01ce961da9f083a896249ce218f2dc0a
Zatchs <j.martin@simplon-charleville.fr>
2019-05-07 16:01:11 +0200

    Ajout des pages pdf en lecture html

---
+++ tarif-d.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Index</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/reglement.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    <?php include('header.php');?>
+    <div class="espace"></div>
+    <div class="espace"></div>
+    <div class="espace"></div>
+    <iframe class="pdf" src="pdf/tarif-d-2018.pdf" height="1200px" width="1600px"></iframe>
+    <div class="espace"></div>
+    <div class="espace"></div>
+    <?php include('footer.php');?>
+
+    </body>
+    </html>
+    
