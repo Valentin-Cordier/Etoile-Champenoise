1710cdfb507688f5324f4c93f173236189510631
jtonolo <jules.tonolo@gmail.com>
2019-05-21 16:47:14 +0200

    index front jules

--- index.php
+++ index.php
@@ -4,24 +4,24 @@
 <head>
     <title>Etoile Champenoise 2019</title>
     <!--FAVICON-->
-  <link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="images/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-<link rel="manifest" href="/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
-<!--END FAVICON-->
+    <link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <!--END FAVICON-->
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
@@ -32,7 +32,7 @@
     </script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
@@ -40,155 +40,105 @@
     <Center>
         <div class="intro">
 
-        <div class="espace"></div>
+            <div class="espace"></div>
 
             <div id="logo" class="mb-5 mx-auto text-center">
                 <img class="mt-5" src="images/logo.png" width="300px" alt="logo étoile champenoise">
             </div>
 
-            <div class="espace"></div>
 
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <h3 style="color:rgb(196, 155, 43)" class="col-6 mx-auto mb-5">Une porte ouverte sur L'Europe: 
+                        <span style="color:white"><strong>HUB</strong></span> de transit européen pour la <span style="color:white"><strong>MESSAGERIE</strong></span>
+                         et les <span style="color:white"><strong>PALETTES</strong></span></h3>
                         <h4> 51520 RECY, plate-forme au </h4>
                         <h4> coeur de l'Europe </h4>
+                       
                         <div class="espace"></div>
-                        <a href="etoile-prive/loggin.php"><button type="button" class="btn btn-outline-primary btn-lg">Se
+                        <a href="etoile-prive/loggin.php"><button type="button"
+                                class="btn btn-outline-primary btn-lg">Se
                                 connecter</button></a>
                         <div class="espace"></div>
                     </div>
                 </div>
             </div>
-        </div>
+</div>
     </center>
+    <div class="row pb-5 pt-5 d-flex justify-content-around text-light" style="background: rgb(31, 39, 92)">
+        <div class="ite text-center mt-3 col-lg-3 col-sm-12">
+            <div class="supit mb-4 mx-auto">
 
-    <div class="parallax-window" data-parallax="scroll" data-image-src="images/5.jpg" alt="Competences">
-        <div class="container-fluid">
+                <i class="far fa-check-circle fa-3x mt-5"></i>
+            </div>
+            <h4 class="mb-3" style="font-size:2em;color:rgb(196, 155, 43)">Simplicité</h4>
 
-            <div class="espace"></div>
+            <p class="col-lg-12 col-sm-6 mx-auto">Sûreté et traçabilité de vos expéditions grâce à un
+                étiquetage code-barres Norme GTF Code INOVERT</p>
 
+        </div>
+        <div class="ite text-center mt-3 col-lg-3 col-sm-12">
+            <div class="supit mb-4 mx-auto">
+                <i class="fas fa-award fa-3x mt-5"></i>
+            </div>
+            <h4 class="mb-3" style="font-size:2em;color:rgb(196, 155, 43)">Fiabilité</h4>
+
+            <p class="col-lg-12 col-sm-6 mx-auto">Contrôle de la qualité par un process unique et commun assurant un
+                suivi efficace</p>
+
+        </div>
+        <div class="ite text-center mt-3 col-lg-3 col-sm-12">
+            <div class="supit mb-4 mx-auto">
+                <i class="fas fa-shipping-fast fa-3x mt-5 "></i>
+            </div>
+            <h4 class="mb-3" style="font-size:2em;color:rgb(196, 155, 43)">Rapidité</h4>
+            <p class="col-lg-12  col-sm-6 mx-auto">Contrôle de la qualité par un process unique et commun assurant un
+                suivi efficace</p>
+
+        </div>
+    </div>
+   <!---------------------------------------------- ETOILES APRES 3 FENETRES------------------>
+   
+
+<div class='lol'>
+    <div class='image-left'>
+        <div class="container-fluid lol">
             <div class="row">
-                <!-- Sureté -->
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 align-self-center">
+                    <div class="carre-blanc rounded p-5">
+                        <h2 class="infos mb-4">
+                            <span class="infos-upper">Au 01/01/2015
+                            <span class="infos-lower"><i><b><span style="color:rgb(196, 155, 43);">l'ETOILE
+                                        Champenoise
+                                    </span></b></i> c'est : </span>
 
-                <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12">
-                </div>
-
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 shadow p-3 mb-5 bg-white rounded atouts">
-                    <div class="atouts text-center">
-                        <i class="far fa-check-circle fa-3x"></i>
-
-                        <h4>Simplicité</h4>
-                        <div class="espace2"></div>
-                        <p>Sûreté et traçabilité de vos expéditions grâce à un
-                            étiquetage code-barres Norme GTF Code INOVERT</p>
-
+                        </h2>
+                        <p class="texteDiagonale"><i class="fas fa-warehouse icone-caracteristique"></i>
+                            6750 mètres carrés
+                        </p>
+                        <p class="texteDiagonale"><i class="fas fa-balance-scale icone-caracteristique"></i>
+                            300 tonnes de marchandises traitées chaque nuit</p>
+                        <p class="texteDiagonale"><i class="fas fa-truck icone-caracteristique"></i>
+                            2500 expéditions au quotidien </p>
                     </div>
-                </div>
-                <!-- Fiabilité -->
-                <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12">
-                </div>
-                <div class="col-xl-2 col-lg-2 col-md-2 col-sm-12 col-12 shadow p-3 mb-5 bg-white rounded atouts">
-                    <div class="atouts text-center">
-                        <i class="fas fa-award fa-3x"></i>
-
-                        <h4>Fiabilité</h4>
-                        <div class="espace2"></div>
-                        <p>Transit ADR assuré par du personnel formé à la
-                            gestion de matières dangereuses en colis (sauf classe 1 ; 6.2 et 7)</p>
-                    </div>
-                </div>
-                <!-- Raidité -->
-                <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12">
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 shadow p-3 mb-5 bg-white rounded atouts">
-                    <div class="atouts text-center">
-                        <i class="fas fa-shipping-fast fa-3x"></i>
-
-                        <h4>Rapidité</h4>
-                        <div class="espace2"></div>
-                        <p>Contrôle de la qualité par un process unique et commun
-                            assurant un suivi efficace</p>
-                    </div>
-                </div>
-                <div class="col-xl-1 col-lg-1 col-md-1 col-sm-12 col-12">
                 </div>
             </div>
         </div>
     </div>
+    <div class='image-right'></div>
+</div>
 
-    <!---------------------------------------------- ETOILES APRES 3 FENETRES------------------>
-
-
-
-    <div class="espace"></div>
-    <div class="espace"></div>
-
-    <center>
-        <div class="container-fluid ">
-            <div class="row">
-                <div class="col-xl-1 col-lg-1 col-md-12 col-sm-12 col-12">
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 shadow p-3 mb-5 bg-white rounded">
-                    <i class="far fa-star icone-caracteristique-etoile"></i>
-                    <div class="espace2"></div>
-                    <h3 class="separesouligne"> Au 01/01/2015<br> <span style="color:rgb(196, 155, 43);">l'ETOILE
-                            Champenoise </span> c’est : </h3>
-                    </h3>
-
-                    <div class="espace2"></div>
-
-                    <p class="separe"><i class="fas fa-warehouse icone-caracteristique"></i> 6750 mètres carrés </p>
-
-                    <div class="espace3"></div>
-
-                    <p class="separe"><i class="fas fa-balance-scale icone-caracteristique"></i> 300 tonnes de
-                        marchandises
-                        traitées chaque nuit</p>
-
-                    <div class="espace3"></div>
-
-                    <p class="separe"><i class="fas fa-truck icone-caracteristique"></i> 2500 expéditions au
-                        quotidien </p>
-
-                </div>
-                <div class="col-xl-1 col-lg-1 col-md-12 col-sm-12 col-12">
-                </div>
-                <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12 shadow lg-3 mb-5 bg-white rounded">
-                    <div class="hovereffect">
-                        <img class="entrepot" src="images/4.jpg">
-                        <div class="overlay">
-                            <h2>NOS LOCAUX</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-1 col-lg-1 col-md-12 col-sm-12 col-12">
-                </div>
-            </div>
-        </div>
-
-        <div class="espace"></div>
-        <div class="espace"></div>
-
-        <!------------------------- Parallaxe ---------------------->
-        <div class="parallax-window" data-parallax="scroll" data-image-src="images/mbsP.png" alt="Competences">
-        </div>
-
-        <div class="espace"></div>
+<div class="row d-flex">
+<img class="col-6 h-100 img-fluid" src="images/carte.png">
+<div>
+    <
+</div>
 
 
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                            <img class="image-fluid carte" src="images/carte.png">
-                </div>
-            </div>
-        </div>
-    </center>
+ 
 
-    <div class="espace"></div>
-    <div class="espace"></div>
-
+  
 </body>
 
 <?php include('footer.php'); ?>
